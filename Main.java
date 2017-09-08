472cd1c8ebfee75d0b1a122de30b4004e56dd148
BurriSwathi <31738799+BurriSwathi@users.noreply.github.com>
2017-09-08 16:17:56 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+	public static void main(String[] args) {
+		int length;
+		private string fullname;
+		int age
+		//You may test that your code works find here
+		//Please check that your code works and has no 
+		//compilation problems before to submit
+	}
+
+}
